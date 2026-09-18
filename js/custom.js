f428171b38699b7ee896d006e92602ed3667f126
oichristkouame920 <oichristkouame920@gmail.com>
2026-09-18 14:51:56 +0000

    amelioration de la presentation generale et du visuel

--- js/custom.js
+++ js/custom.js
@@ -1,48 +1,124 @@
 (function ($) {
+
   "use strict";
-    // NAVBAR
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
+
+
+  // NAVBAR
+  $('.navbar-collapse a').on('click', function () {
+
+    $(".navbar-collapse").collapse('hide');
+
+  });
+
+
+  // IMAGES DE LA PAGE D'ACCUEIL
+  $(function () {
+
+    var imagesAccueil = [
+
+      "images/entrelogi.png",
+      "images/entrepot1.avif",
+      "images/entrepo2.avif",
+      "images/chargeme.png",
+      "images/hamburg-6849995_1280.jpg",
+      "images/camionslogi.png",
+      "images/interieur/02_preparation_commandes_02.jpg"
+
+    ];
+
+
+    /*
+     * Vérifie que chaque image existe
+     * avant de l'envoyer à Vegas.
+     */
+    var verifications = imagesAccueil.map(function (chemin) {
+
+      return new Promise(function (resolve) {
+
+        var image = new Image();
+
+        image.onload = function () {
+
+          resolve({
+            src: chemin
+          });
+
+        };
+
+
+        image.onerror = function () {
+
+          console.warn(
+            "Image ignorée car introuvable ou invalide :",
+            chemin
+          );
+
+          resolve(null);
+
+        };
+
+
+        image.src = chemin;
+
+      });
+
     });
 
-    $(function() {// pour les images qui sont sur la page d'acceuil
-      $('.hero-slides').vegas({ 
-          slides: [
-            {src:"images/entrelogi.png"},
-            {src:"images/entrepot1.avif"},
-            {src:"images/entrepo2.avif"},
-            {src:"images/chargeme.png"},
-            {src:"images/hamburg-6849995_1280.jpg"},
-            {src:"images/camionslogi.png"},
-<<<<<<< HEAD
-            {src:"images/termi.jpg"} 
-=======
-            {src:"images/entrepot-dakar.jpg"}.
->>>>>>> origin/main
-          ],
-          timer: false,
-          animation: 'kenburns',
+
+    Promise.all(verifications).then(function (resultats) {
+
+      /*
+       * Retire les images qui n'ont pas pu
+       * être chargées.
+       */
+      var slidesValides = resultats.filter(function (slide) {
+
+        return slide !== null;
+
+      });
+      if (slidesValides.length === 0) {
+
+        console.error(
+          "Aucune image valide trouvée pour le carrousel LOGITIX."
+        );
+
+        return;
+
+      }
+      /*
+       * Lancement Vegas
+       */
+      $('.hero-slides').vegas({
+        slides: slidesValides,
+        autoplay: true,
+        loop: true,
+        delay: 5000,
+        timer: false,
+        transition: 'fade',
+        transitionDuration: 1200,
+        animation: 'kenburns',
+        animationDuration: 7000
       });
     });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height() + 60;
-  
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
+  });
+  // CUSTOM LINK
+  $('.smoothscroll').click(function () {
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height() + 60;
+    scrollToDiv(
+      elWrapped,
+      header_height
+    );
+    return false;
+    function scrollToDiv(element, navheight) {
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll =
+        offsetTop - navheight;
+      $('body,html').animate({
         scrollTop: totalScroll
-        }, 300);
-      }
-    });
-  
-  })(window.jQuery);
+      }, 300);
+    }
+  });
+})(window.jQuery);
